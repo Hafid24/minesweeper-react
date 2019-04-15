74a0fa783b3fe039f0a62929a5ffea81872e950a
Hafid24 <ilymassh@gmail.com>
2019-04-15 05:43:01 -0700

    adding open function

--- src/components/Board.js
+++ src/components/Board.js
@@ -39,13 +39,59 @@ export default class Board extends Component {
         }
         return board; 
     }
+     
+  open = cell => {
+    if (this.props.status === "ended") {
+      return;
+    }
+   
+    let asyncCountMines = new Promise(resolve => {
+      let mines = this.findMines(cell);
+      resolve(mines);
+    });
+
+    asyncCountMines.then(numberOfMines => {
+      let rows = this.state.rows;
+
+      let current = rows[cell.y][cell.x];
+
+      if (current.hasMine && this.props.openCells === 0) {
+        console.log("mine was on first click");
+        let newRows = this.createBoard(this.props);
+        this.setState({ rows: newRows }, () => {
+          this.open(cell);
+        });
+      } else {
+        if (!cell.hasFlag && !current.isOpen) {
+          this.props.onCellClick();
+
+          current.isOpen = true;
+          current.count = numberOfMines;
+
+          this.setState({ rows });
+          // now that we know its not a flag and its not a BOMB we should try to open cells around it!
+          if (!current.hasMine && numberOfMines === 0) {
+            this.openAroundCell(cell);
+          }
+
+          if (current.hasMine && this.props.openCells !== 0) {
+            this.props.endGame();
+          }
+        }
+      }
+    });
+  };
+
+
+
   render() {
     let rows = this.state.rows.map(( row,index) =>{
       return <Row cells = {row} key={index}/> 
   
     }) 
     return (
-      <div>
+      <div className="board">
+        {rows}
         
       </div>
     )
