398c17c6325a3476bf057e004cce4b4f6ce1ffff
Hafid24 <ilymassh@gmail.com>
2019-04-15 12:01:19 -0700

    add continuous deployment to sursh.sh using travis

---
+++ build/precache-manifest.bf0f6802b50f7a30397a607581ea0cb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "648490299b62ae25bbd0",
+    "url": "/static/js/main.64849029.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "648490299b62ae25bbd0",
+    "url": "/static/css/main.c872c371.chunk.css"
+  },
+  {
+    "revision": "2f70ce3fe3979cbcfa45117337e63b9f",
+    "url": "/index.html"
+  }
+];
